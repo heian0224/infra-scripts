8b1a877678c080f93c37325a68e8948e15e044c2
Liu Cong <heian0224@gmail.com>
2025-04-15 00:27:31 +0800

    update certbot shell

--- record/python-certbot.sh
+++ record/python-certbot.sh
@@ -12,7 +12,9 @@ sudo ln -s /opt/certbot/bin/certbot /usr/bin/certbot
  
 sudo certbot certonly --nginx --nginx-ctl /usr/local/nginx/sbin/nginx --nginx-server-root /usr/local/nginx/conf
 
-echo "0 0,12 * * * root /opt/certbot/bin/python -c 'import random; import time; time.sleep(random.random() * 3600)' && sudo certbot renew --post-hook 'systemctl reload nginx' " | sudo tee -a /etc/crontab > /dev/null
+echo "0 0,12 * * * root /opt/certbot/bin/python -c 'import random; import time; time.sleep(random.random() * 3600)' && sudo certbot renew --post-hook 'systemctl reload nginx' " >>   /var/log/certbot/renew.log 2>&1
 # Certificate is saved at: /etc/letsencrypt/live/futurefantasy.tech/fullchain.pem
 # Key is saved at:         /etc/letsencrypt/live/futurefantasy.tech/privkey.pem
 # cron log /var/log/cron
+
+ # /opt/certbot/bin/python -c 'import random; import time; time.sleep(2)' && sudo certbot renew --post-hook 'systemctl reload nginx' >>   "/var/log/certbot/renew_$(date "+%Y_%m_%d_%H_%M_%S").log" 2>&1
